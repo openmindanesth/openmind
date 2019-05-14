6747991ed58356f159d45f9854cd1dbc6d1c6048
Thomas Getgood <thomas.getgood@gmail.com>
2019-05-14 17:46:41 -0400

    Removed elastich dependency.
    Seems quite outdated. I'm going to go with the rest api to start.

--- src/clj/openmind/elastic.clj
+++ src/clj/openmind/elastic.clj
@@ -1,8 +1,5 @@
 (ns openmind.elastic
-  (:require [clojurewerkz.elastisch.rest :as es]
-            [openmind.env :as env]))
+  (:require [openmind.env :as env]))
 
 (def conn
-  (es/connect (env/read :elastic-url)
-              {:basic-auth [(env/read :elastic-username)
-                            (env/read :elastic-password)]}))
+  )
