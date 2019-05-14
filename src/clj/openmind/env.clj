5fac6f291f8a8525508894c01586aefe70787858
Thomas Getgood <thomas.getgood@gmail.com>
2019-05-14 10:46:44 -0400

    Configuration.

--- src/clj/openmind/env.clj
+++ src/clj/openmind/env.clj
@@ -1,10 +1,33 @@
-(ns openmind.env)
+(ns openmind.env
+  (:refer-clojure :exclude [read]))
 
-(def ^:dynamic *dev?*
-	(System/getenv "DEV_MODE"))
+(def env-keys
+  {:elastic-cluster  "ELASTIC_CLUSTER"
+   :elastic-username "ELASTIC_USERNAME"
+   :elastic-password "ELASTIC_PASSWORD"
+   :elastic-hostname "ELASTIC_HOSTNAME"
+   :elastic-url      "ELASTIC_URL"
+   :elastic-port     "ELASTIC_PORT"
+   :dev?             "DEV_MODE"
+   :port             "PORT"})
 
-(def els-user
-	(System/getenv "ELASTIC_USERNAME"))
+(defn read-co []
+  (try
+    (read-string (slurp "conf.edn"))
+    (catch Exception e nil)))
 
-(def els-password
-	(System/getenv "ELASTIC_PASSWORD"))
+(defn read-env []
+  (into {} (comp (map (fn [[k v]] [k (System/getenv v)]))
+                 (remove (fn [[k v]] (nil? v))))
+        env-keys))
+
+;;;; Env vars override config file.
+(def ^:private config
+  (merge (read-config) (read-env)))
+
+(defn read
+  "Read key from environment. key must be defined either in conf.end in the
+  project root or as an env var according to env-keys."
+  [key]
+  (when (contains? config key)
+    (get config key)))
