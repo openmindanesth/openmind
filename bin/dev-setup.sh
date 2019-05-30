90eeb8d3ec6b8fb7b33a3961d45e97b660eab0fb
Thomas Getgood <thomas.getgood@gmail.com>
2019-05-30 12:55:40 -0400

    Don't name the elastic node.
    Hash conflicts are always exciting...

--- bin/dev-setup.sh
+++ bin/dev-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.1.0
+docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.1.0
 
 # I'll use minio to spoof S3 locally for storing figures, but that isn't necessary yet.
 # docker run -p 9000:9000 minio/minio server /data
