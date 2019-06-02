04f3f44559d7f10c67b37540ebd455d2a823e32d
Thomas Getgood <thomas.getgood@gmail.com>
2019-06-02 11:49:58 -0400

    Create default config file if one isn't present.

--- bin/dev-setup.sh
+++ bin/dev-setup.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+if test $(basename `pwd`) = "bin"
+then
+		cd ..
+fi
+
+FILE="conf.edn"
+if ! test -e $FILE
+	 then
+		cat << EOF > $FILE
+{:elastic-url "http://localhost:9200"
+ :port        3003
+ :dev?        true}
+EOF
+fi
+
 docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.1.0
 
 # I'll use minio to spoof S3 locally for storing figures, but that isn't necessary yet.
