4b627f2c6fae36491bf6322c95559f2b22141214
Thomas Getgood <thomas.getgood@gmail.com>
2019-06-08 14:30:14 -0400

    Added logic to reconnect on ws close.

--- src/cljs/openmind/core.cljs
+++ src/cljs/openmind/core.cljs
@@ -1,48 +1,9 @@
 (ns ^:figwheel-hooks openmind.core
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
-  (:require [cljs.core.async :as async]
-            [goog.net.XhrIo]
-            [openmind.config :as config]
+  (:require [openmind.config :as config]
             [openmind.events :as events]
             [openmind.views :as views]
             [re-frame.core :as re-frame]
-            [reagent.core :as reagent]
-            [taoensso.sente :as sente]))
-
-(defmulti ch-handler
-  "Dispatch events from server."
-  :id)
-
-(defmethod ch-handler :default
-  [e]
-  (println "Unknown server event:" e))
-
-(defmethod ch-handler :chsk/state
-  [_]
-  "Connection state change.")
-
-(defmethod ch-handler :chsk/handshake
-  [_]
-  "WS handshake.")
-
-(defmethod ch-handler :chsk/recv
-  [e]
-  (re-frame/dispatch [::events/server-message e]))
-
-(defn connect-chsk []
-  (let [csrf-ch (async/promise-chan)]
-    (goog.net.XhrIo/send "/elmyr"
-                         (fn [e]
-                           (->> e
-                                .-target
-                                .getResponseText
-                                (async/put! csrf-ch))))
-    (go
-      (let [token (async/<! csrf-ch)
-            chsk (sente/make-channel-socket-client!
-                  "/chsk" token {:type :auto})]
-        (sente/start-client-chsk-router! (:ch-recv chsk) ch-handler)
-        (re-frame/dispatch [::events/server-connection chsk])))))
+            [reagent.core :as reagent]))
 
 (defn dev-setup []
   (when config/debug?
@@ -56,7 +17,6 @@
 
 (defn ^:export init []
   (re-frame/dispatch-sync [::events/initialise-db])
+  (re-frame/dispatch [::events/search-request])
   (dev-setup)
-  (mount-root)
-  (connect-chsk)
-  (re-frame/dispatch [::events/server-search-request]))
+  (mount-root))
--- src/cljs/openmind/events.cljs
+++ src/cljs/openmind/events.cljs
@@ -1,17 +1,43 @@
 (ns openmind.events
-  (:require
-   [re-frame.core :as re-frame]
-   [openmind.db :as db]))
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require [cljs.core.async :as async]
+            [goog.net.XhrIo]
+            [re-frame.core :as re-frame]
+            [openmind.db :as db]
+            [taoensso.sente :as sente]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; WS router
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmulti ch-handler
+  "Dispatch events from server."
+  :id)
+
+(defmethod ch-handler :default
+  [e]
+  (println "Unknown server event:" e))
+
+(defmethod ch-handler :chsk/state
+  [_]
+  "Connection state change.")
+
+(defmethod ch-handler :chsk/handshake
+  [_]
+  "WS handshake.")
+
+(defmethod ch-handler :chsk/recv
+  [e]
+  (re-frame/dispatch [::server-message e]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Events
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (re-frame/reg-event-db
  ::initialise-db
  (fn [_ _]
    db/default-db))
-
-(re-frame/reg-event-db
- ::server-connection
- (fn [db [_ chsk]]
-   (assoc db :chsk chsk)))
 
 (re-frame/reg-event-db
  ::toggle-edit
@@ -34,21 +60,24 @@
  (fn [db [_ sel]]
    (assoc db :filter sel)))
 
-(re-frame/reg-event-db
- ::results
- (fn [db [_ {:keys [results nonce] :as e}]]
-   (if (< (get-in db [:search :response-number]) nonce)
-     (-> db
-         (assoc-in [:search :response-number] nonce)
-         (assoc :results results))
-     db)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Server Comms
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: real indexing event
+
+(defn index-doc [send-fn doc]
+  (send-fn [:openmind/index doc]))
+
+(defn unsafe-index [doc]
+  (index-doc (:send-fn @re-frame.db/app-db) doc))
 
 (defn reg-search-updater [key update-fn]
   (re-frame/reg-event-fx
    key
    (fn [cofx e]
      {:db       (update-fn (:db cofx) e)
-      :dispatch [::server-search-request]})))
+      :dispatch [::search-request]})))
 
 (reg-search-updater
  ::search
@@ -66,32 +95,75 @@
    (update-in db [:search :filters feature] disj value)))
 
 (re-frame/reg-event-fx
- ::server-search-request
+ ::search-request
  (fn [cofx _]
    (let [search  (get-in cofx [:db :search])
-         search  (update search :nonce inc)
-         chsk    (get-in cofx [:db :chsk])
-         send-fn (:send-fn chsk)
-         open?   (when-let [s (:state chsk)] (:open? @s))
-         user    (get-in cofx [:db :user])]
-     (if open?
-       {:db           (assoc (:db cofx) :search search)
-        ::send-search {:search search :send-fn send-fn :user user}}
-       {:dispatch-later [{:ms 100 :dispatch [::server-search-request]}]}))))
+         search  (update search :nonce inc)]
+     {:db        (assoc (:db cofx) :search search)
+      :dispatch [::try-send [:openmind/search {:search search}]]})))
+
+(re-frame/reg-event-db
+ :openmind/search-response
+ (fn [db [_ {:keys [results nonce] :as e}]]
+   (if (< (get-in db [:search :response-number]) nonce)
+     (-> db
+         (assoc-in [:search :response-number] nonce)
+         (assoc :results results))
+     db)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Connection management
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(re-frame/reg-event-fx
+ ::try-send
+ (fn [{{:keys [chsk user]} :db} [_ ev]]
+   (if (and (satisfies? IDeref (:state chsk))
+            (:open? @(:state chsk)))
+     {::send! {:ev ev :send-fn (:send-fn chsk) :user user}}
+     {::connect-chsk! true
+      :dispatch       [::enqueue-request ev]})))
+
+(re-frame/reg-event-db
+ ::enqueue-request
+ (fn [db [_ ev]]
+   (update db :request-queue conj ev)))
 
 (re-frame/reg-fx
- ::send-search
- (fn [{:keys [send-fn] :as req}]
-   (send-fn [:openmind/search (dissoc req :send-fn)]
-            10000
-            ;; TODO: When we have logged in users, we should use their user
-            ;; ids. I don't know how to do that for anonymous users though
-            ;; (random IDs, I suppose, but this is just as good).
-            (fn [[_ res]]
-              (re-frame/dispatch [::results res])))))
+ ::send!
+ (fn [{:keys [send-fn ev user]}]
+   ;; FIXME: For mutli-device logged in users, this will duplicate responses.
+   (send-fn ev 10000 re-frame/dispatch)))
 
-(defn index-doc [send-fn doc]
-  (send-fn [:openmind/index doc]))
 
-(defn unsafe-index [doc]
-  (index-doc (:send-fn @re-frame.db/app-db) doc))
+(re-frame/reg-event-fx
+ ::server-connection
+ (fn [{:keys [db]} [_ chsk]]
+   (let [pending (:request-queue db)]
+     (merge {:db (assoc db :chsk chsk :request-queue [] :connecting? false)}
+            (when (seq pending)
+              {:dispatch-n (mapv (fn [ev] [::try-send ev]) pending)})))))
+
+(let [connecting? (atom false)]
+  (re-frame/reg-fx
+   ::connect-chsk!
+   (fn [_]
+     (when-not @connecting?
+       (reset! connecting? true)
+       (println "Connecting to server...")
+       (let [csrf-ch (async/promise-chan)]
+         (goog.net.XhrIo/send "/elmyr"
+                              (fn [e]
+                                (->> e
+                                     .-target
+                                     .getResponseText
+                                     (async/put! csrf-ch))))
+         (go
+           (let [token (async/<! csrf-ch)
+                 chsk  (sente/make-channel-socket-client!
+                        "/chsk" token {:type :auto})]
+             ;; Wait for a message so that we know the channel is open.
+             (async/<! (:ch-recv chsk))
+             (reset! connecting? false)
+             (sente/start-client-chsk-router! (:ch-recv chsk) ch-handler)
+             (re-frame/dispatch [::server-connection chsk]))))))))
