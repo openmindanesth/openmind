a351afaa116a4e5fd3fed33be8e4bf2b779dbfac
Thomas Getgood <thomas.getgood@gmail.com>
2019-06-13 15:02:02 -0400

    Not using minio at present.

--- bin/dev-setup.sh
+++ bin/dev-setup.sh
@@ -19,4 +19,4 @@ fi
 docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.1.0
 
 # I'll use minio to spoof S3 locally for storing figures, but that isn't necessary yet.
-docker run -d -p 9000:9000 minio/minio server /data
+# docker run -d -p 9000:9000 minio/minio server /data
