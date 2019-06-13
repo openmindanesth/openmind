c141643e06d172de49faad12d82a7d3074bca1e5
Thomas Getgood <thomas.getgood@gmail.com>
2019-06-13 15:34:51 -0400

    Don't need the dummy data any longer.

--- src/cljs/openmind/db.cljs
+++ src/cljs/openmind/db.cljs
@@ -1,29 +1,6 @@
 (ns openmind.db
   (:require [openmind.search :as search]))
 
-(def dummy-results
-  [{:text "Medetomidine has no dose-dependent effect on the BOLD response to subcutaneous electrostimulation (0.5, 0.7, 1 mA) in mice for doses of 0.1, 0.3, 0.7, 1.0, 2.0 mg/kg/h."
-    :reference "Nasrallah et al., 2012"
-    :tags
-    {:type :extract
-     :species :human
-     :modality :cortex
-     :depth :moderate}}
-   {:text "Medetomidine has been shown to promote vasoconstriction in rats measured by decrease in central arterial diameter."
-    :reference "Another et al., 1996"
-    :tags
-    {:type :extract
-     :species :mouse
-     :modality :cortex
-     :depth :moderate}}
-   {:text "BOLD responses under medetomidine are attenuated and onset is delayed in mechanically ventilated mice."
-    :reference "Schroeter et al., 2014"
-    :tags
-    {:type :extract
-     :species :human
-     :modality :cortex
-     :depth :moderate}}])
-
 (def empty-filters
   (into {} (map (fn [[k v]] [k #{}])) search/filters))
 
