96dffee876929e2965c261f6f843311e135cc63e
Thomas Getgood <thomas.getgood@gmail.com>
2019-09-08 14:11:37 -0300

    Restored the static tag tree to the codebase.

--- src/clj/openmind/tags.clj
+++ src/clj/openmind/tags.clj
@@ -3,6 +3,45 @@
             [clojure.data.json :as json]
             [openmind.elastic :as es]))
 
+(def tag-tree
+  "For development purposes, I'm encoding the tress of tags here. Once we've got
+  the system up and running it will need to be an interactive search through
+  elastic. Something like graphiql (like graphiql) would be great."
+  {"anaesthesia" {"species"     {"human"  {}
+                                 "monkey" {}
+                                 "rat"    {}
+                                 "mouse"  {}}
+                  "modality"    {"neuron level"       {}
+                                 "ensemble (LFP)"     {}
+                                 "cortex (EEG)"       {}
+                                 "large scale (fMRI)" {}}
+                  "application" {"sensory stimuli"   {"visual"        {}
+                                                      "auditory"      {}
+                                                      "olfactory"     {}
+                                                      "somatosensory" {}
+                                                      "nociceptive"   {}}
+                                 "resting state"     {}
+                                 "brain stimulation" {"deep brain"    {}
+                                                      "optogenetics"  {}
+                                                      "chemogenetics" {}}}
+                  "anaesthetic" {"GABAergic"         {"propofol"        {}
+                                                      "editomidate"     {}
+                                                      "benzodiazepines" {}
+                                                      "barbitol"        {}}
+                                 "volatile ethers"   {"isoflurane"  {}
+                                                      "sevoflurane" {}}
+                                 "a2 AR antagonists" {"(dex)metedetomidine" {}
+                                                      "xylazine"            {}}
+                                 "NMDA antagonists"  {"ketamine" {}}}
+                  "depth"       {"light"    {}
+                                 "moderate" {}
+                                 "deep"     {}}
+                  "physiology"  {"blood pressure" {}
+                                 "heart rate"     {}
+                                 "hemodynamics"   {}
+                                 "breathing rate" {}
+                                 "other effects"  {}}}})
+
 (defonce
   ^{:private true
     :doc "Top level tag domains. These are presently invisible to the client since the
