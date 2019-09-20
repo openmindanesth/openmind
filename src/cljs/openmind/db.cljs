7eeb7b163224f32bf558434ed8b7fed516237c9b
Thomas Getgood <thomas.getgood@gmail.com>
2019-09-20 10:47:21 -0400

    Deleted hopelessly out of date db specs.

--- src/cljs/openmind/db.cljs
+++ src/cljs/openmind/db.cljs
@@ -7,30 +7,4 @@
    :tag-lookup                     ::uninitialised
    ::status-message                 ""
    :openmind.components.tags/search {:search/selection []}
-   :openmind.router/route           nil
-   :openmind.components.extract-editor/new-extract
-   {:new-extract/selection                      []
-    :openmind.components.extract-editor/content {:comments  {0 ""}
-                                                 :related   {0 ""}
-                                                 :contrast  {0 ""}
-                                                 :confirmed {0 ""}
-                                                 :figures   {0 ""}
-                                                 :tags      #{}}
-    :errors                                     nil}
-   ::results                        []})
-
-(s/def ::db
-  (s/keys :req [::new-extract
-                ::tag-tree
-                ::route
-                ::results
-                ::current-search]))
-
-(s/def ::new-extract
-  (s/keys :req [:new-extract/selection :new-extract/content]))
-
-(s/def :new-extract/selection
-  (s/coll-of string? :distinct true))
-
-(s/def :new-extract/content
-  (s/keys :req []))
+   :openmind.router/route           nil})
