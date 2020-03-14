a3e6f8599777db1f6ffb0fd9620d4b7e645df996
Thomas Getgood <thomas.getgood@gmail.com>
2020-03-14 12:40:16 -0400

    Specs and notes for indexing.

--- src/clj/openmind/indexing.clj
+++ src/clj/openmind/indexing.clj
@@ -1,6 +1,38 @@
 (ns openmind.indexing
   (:require [clojure.spec.alpha :as s]
-            [openmind.spec :as spec]))
+            [openmind.s3 :as s3]
+            [openmind.spec :as spec]
+            [openmind.spec.indexical :as ind]))
+
+(def master-index
+  {:active-extracts []
+   :extract-comments {}
+   :extract-relations {}
+   :rav {}})
+
+;; I need an index that tracks
+;; comment -> refers-to(of :comment spec) -> all views involving comment
+
+;; so a value->attribute->view index
+
+;; The idea is that when I get a new comment, it :refers-to the thing it's
+;; about. I need to be able to invert that link and insert this new comment into
+;; a tree. Which tree is something I don't yet know.
+;;
+;; So the index gives me all views the comment applies to. I look at those and
+;; pick the ones I want to insert the comment into. Then I walk the tree and
+;; find the appropriate place and update the tree. Save the new tree as an
+;; immutable, update the extract comments table, and update the master index.
+;;
+;; Updating the master index requires atomic compare and swap. The rest does
+;; not, but if the CAS fails, then the whole transaction has to replay because
+;; we don't know where in the tree the conflict was. On the up side, if the
+;; conflict was high up in the tree, then we can use memoisation to avoid
+;; redoing work unnecessarily.
+;;
+;; Of course this will result in orphan nodes in the datastore. I'm not
+;; convinced that's a bad thing. GC is possible, but only if you can syncronise
+;; all past readers.
 
 (defmulti update-indicies (fn [t d] t))
 
@@ -9,6 +41,6 @@
     (update-indicies t imm)))
 
 (defmethod update-indicies :comment
-  [_ {:keys [hash content]}]
+  [_ {:keys [hash content] :as comment}]
   (let [{:keys [refers-to]} content]
     (println refers-to)))
--- src/cljc/openmind/spec/indexical.cljc
+++ src/cljc/openmind/spec/indexical.cljc
@@ -11,4 +11,30 @@
 
 (s/def ::indexical
   (s/or
-   :tag-lookup-table ::tag-lookup))
+   :tag-lookup-table ::tag-lookup
+   :master ::master-index
+   :comment-tree ::comment-tree))
+
+(s/def ::master-index
+  (s/keys :req-un [::active-extracts
+                   ::extract-comments
+                   ::extract-relations]))
+
+(s/def ::active-extracts
+  (s/coll-of ::u/hash :distinct true))
+
+(s/def ::extract-comments
+  (s/map-of ::u/hash ::u/hash))
+
+(s/def ::extract-relations
+  (s/map-of ::u/hash ::u/hash))
+
+(s/def ::comment-tree
+  (s/coll-of ::comment :distinct true))
+
+(s/def ::comment
+  (s/keys :req-un [::u/text ::u/author ::rank ::replies ::u/hash]))
+
+(s/def ::replies ::comment-tree)
+
+(s/def ::rank int?)
