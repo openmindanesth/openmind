eeef9b59177e9db7824d20a6acb3813f86aa436e
Thomas Getgood <thomas.getgood@gmail.com>
2020-03-19 16:59:28 -0400

    Fixed figure page to use new :content sub.

--- src/cljs/openmind/components/extract.cljs
+++ src/cljs/openmind/components/extract.cljs
@@ -7,12 +7,12 @@
 (defn figure-page
   [{{:keys [id] :or {id ::new}} :path-params}]
   (let [id                (edn/read-string id)
-        {:keys [figures]} @(re-frame/subscribe [:extract/content id])]
+        {:keys [figures]} @(re-frame/subscribe [:content id])]
     (if (seq figures)
       (into [:div]
             (map (fn [fid]
                    (let [{:keys [image-data caption]}
-                         @(re-frame/subscribe [:extract/content fid])]
+                         @(re-frame/subscribe [:content fid])]
                      [:div
                       [:img.p2 {:style {:max-width "95%"} :src image-data}]
                       [:p.pl1.pb2 caption]])))
