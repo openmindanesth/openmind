2f600309126ad94913d13f122912995372a9bdd3
Thomas Getgood <thomas.getgood@gmail.com>
2020-03-18 22:22:13 -0400

    Added skeleton comment view.

--- src/cljs/openmind/components/extract.cljs
+++ src/cljs/openmind/components/extract.cljs
@@ -38,14 +38,24 @@
 
 (re-frame/reg-event-fx
  ::save-new-comment
- (fn [{:keys [db]} [_ id]]
-   (let [{:keys [text]} (get-comment db id)]
+ (fn [{:keys [db]} [_ page-id reply-id]]
+   (let [id             (or reply-id page-id)
+         {:keys [text]} (get-comment db id)]
      (if (empty? text)
        {:db (assoc-in db [::new-comments id :errors] "Comments can't be blank.")}
        (let [author  (:login-info db)
-             comment (util/immutable {:text text :author author :refers-to id})]
+             comment (merge
+                      {:text text :author author :extract page-id}
+                      (when reply-id
+                        {:reply-to reply-id}))]
          ;; TODO: Clear comment entry area on successful intern
-         {:dispatch [:openmind.events/try-send [:openmind/intern comment]]})))))
+         {:dispatch       [:openmind.events/try-send
+                           [:openmind/intern (util/immutable comment)]]
+          :dispatch-later [{:ms       300
+                            :dispatch [:openmind.events/try-send
+                                       [:openmind/extract-metadata page-id]]}]
+          :db             (assoc-in db [::new-comments id] {})})))))
+
 
 (re-frame/reg-event-fx
  ::revalidate
@@ -54,8 +64,9 @@
      (when (seq text)
        {:db (assoc-in db [::new-comments id :errors] nil)}))))
 
-(defn new-comment [id]
-  (let [{:keys [text errors]} @(re-frame/subscribe [::new-comment id])]
+(defn new-comment [page-id reply-id]
+  (let [id (or reply-id page-id)
+        {:keys [text errors]} @(re-frame/subscribe [::new-comment id])]
     [:div.flex.full-width
      [:div.full-width.pr1
       [:textarea.full-width-textarea
@@ -67,9 +78,8 @@
                       (let [v (-> ev .-target .-value)]
                         (when errors
                           (re-frame/dispatch [::revalidate id]))
-                        (re-frame/dispatch [::new-comment id v])))}
-        (when text
-          {:value text})
+                        (re-frame/dispatch [::new-comment id v])))
+         :value text}
         (when errors
           {:class "form-error"}))]
       (when errors
@@ -78,20 +88,67 @@
       {:style {:max-height "4rem"
                :right 0}
        :on-click (fn [_]
-                   (re-frame/dispatch [::save-new-comment id]))}
+                   (re-frame/dispatch [::save-new-comment page-id reply-id]))}
       "COMMENT"]]))
+
+(re-frame/reg-sub
+ ::comments
+ (fn [[_ id]]
+   (re-frame/subscribe [:extract-metadata id]))
+ (fn [meta _]
+   (:comments (:content meta))))
+
+(def dateformat
+  (new (.-DateTimeFormat js/Intl) "en-GB"
+       (clj->js {:year   "numeric"
+                 :month  "long"
+                 :day    "numeric"
+                 :hour   "numeric"
+                 :minute "numeric"})))
+
+(defn comment-box [extract-id
+                   {:keys [text time/created replies author hash rank]}]
+  [:div.break-wrap.ph.mbh.flex.flex-column
+   {:key hash}
+   [:div.flex [:div.flex.flex-centre
+               [:div.plh
+                {:on-click #()
+                 :style    {:cursor :pointer
+                            :padding-right "0.05rem"
+                            :margin-right "0.2rem"}}
+                [:b "↑"]]
+               [:div.prh
+                {:on-click #()
+                 :style    {:cursor :pointer
+                            :padding-left "0.05rem"
+                            :margin-left "0.2rem"}}
+                [:b "↓"]]
+               [:span (or rank 0) " votes"]]
+    [:div.pl1
+     [:span.small "by "]
+     [:a.unlink {:href (str "https://orcid.org/" (:orcid-id author))}
+      [:span.text-black.small [:b (:name author)]]]]
+    [:span.pl1 "on " [:em (.format dateformat created)]]
+    [:div.text-dark-grey.ml2
+     {:style {:cursor :pointer}
+      :on-click #()}
+     "reply ⤵"]]
+   [:div.flex.flex-column.ml1
+    [:p text]
+    (when replies
+      (map (partial comment-box extract-id) replies))]])
 
 (defn comments-page
   [{{:keys [id]} :path-params}]
-  (let [comments (:comments @(re-frame/subscribe [:extract/content id]))]
+  (let [id       (edn/read-string id)
+        comments @(re-frame/subscribe [::comments id])]
     [:div.flex.flex-column
-     [new-comment (edn/read-string id)]
+     [new-comment id nil]
+     [:div.vspacer]
      (if (seq comments)
        (into
-        [:div.flex.flex-column.border-round.bg-white.border-solid.p1.pbh]
-        (map (fn [com]
-               [:div.break-wrap.ph.border-round.border-solid.border-grey.mbh
-                com]))
+        [:div.flex.flex-column.p1.pbh]
+        (map (partial comment-box id))
         comments)
        [:span.p2 "No one has commented on this extract yet."])]))
 
