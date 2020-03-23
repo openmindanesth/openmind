148151b5a17cf138a7bb9d9376ed7b1e69b7f467
Thomas Getgood <thomas.getgood@gmail.com>
2020-03-23 14:26:13 -0400

    Added login randomiser for dev work.

--- src/cljs/openmind/dev.cljs
+++ src/cljs/openmind/dev.cljs
@@ -1,9 +1,15 @@
-(ns openmind.dev)
+(ns openmind.dev
+  (:require [re-frame.core :as re-frame]))
 
-(defn fake-login! []
+(defn login! [name id]
   (swap! re-frame.db/app-db update :login-info
          (fn [login]
-           (if (empty? login)
-             {:name "Dev Johnson" :orcid-id "NONE"}
-             login)))
+           {:name name :orcid-id id}))
   nil)
+
+(defn fake-login! []
+  (when (empty? @(re-frame/subscribe [:openmind.subs/login-info]))
+    (login! "Dev Johnson" "NONE")))
+
+(defn random-login! []
+  (login! (str (gensym)) (str (gensym))))
