35d0f6dd5c07be6260801c155728bcaf1b28aa9f
Thomas Getgood <thomas@macroexpanse.com>
2020-06-22 13:15:19 -0400

    Deleted old migration code.
    Bitrot has set in and we're never going to use it again.

--- dev/clj/setup.clj
+++ dev/clj/setup.clj
@@ -42,35 +42,3 @@
          :hits
          (mapv :_source)
          (spit filename))))
-
-;;;;; S3 datastore index init
-
-(def active-extracts-stub
-  [#openmind.hash/ref "723701947793c75d6816580e5b6aa131"
-   #openmind.hash/ref "552fa91af86fc432a292091c0b1331ab"
-
-   #openmind.hash/ref "d594f8e73658cc09a9ab473d08de5095"
-   #openmind.hash/ref "b88867f28b17626b736c19e4e2454ddb"
-   #openmind.hash/ref "96ea2c806cd229176c43e40d001b16ea"])
-
-(def em-stub
-  (mapv (fn [h] (util/immutable {:extract h})) active-extracts-stub))
-
-(def extract-metadata-stub
-  (util/immutable
-   (zipmap active-extracts-stub
-           (map :hash em-stub))))
-
-(defn wipe-metadata!
-  "Creates a new index with a hard-coded set of things and resets the head
-  pointer. Make sure you write down the old one in case you want to go back."
-  []
-  (when-let [current (s3/lookup index/extract-metadata-uri)]
-    (println "Replacing index head: " (:hash current)
-             "with: " (:hash extract-metadata-stub)))
-  ;; TODO: git reset analog
-  (run! s3/intern em-stub)
-  (s3/intern extract-metadata-stub)
-  (@#'s3/index-compare-and-set! index/extract-metadata-uri
-   (s3/lookup index/extract-metadata-uri)
-   extract-metadata-stub))
